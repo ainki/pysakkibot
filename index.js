2c4d9b80b6018897edb67fabbb27dc665744532f
ainki <alexinkilainen@gmail.com>
2018-10-25 19:57:33 +0300

    2.0.0 - Käyttövalmis. Linja komento edennyt.

--- index.js
+++ index.js
@@ -21,41 +21,40 @@ bot.on('text', function (msg) {
 // /start
 bot.on('/start', (msg) => {
     //Lähettää viestin ja näppäimistön
-    bot.sendMessage(msg.from.id, `Hei, ${msg.from.first_name}! Tervetuloa käyttämään pysäkkibottia!\n\nVoit aloittaa käytön kirjoittamalla /hae ja pysäkin nimen tai koodin.\n\nVoit vaihtoehtoisesti myös lähettää sijaintisi ja saada lähistöltäsi seuraavat lähdöt!\n\nJos tarvitset lisää apua tee /help! 😄`, { replyMarkup }); //Vastaa kun käyttäjä käyttää /start komentoa
+    bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Tervetuloa käyttämään pysäkkibottia!\n\nVoit aloittaa käytön kirjoittamalla /hae ja pysäkin nimen tai koodin.\n\nVoit vaihtoehtoisesti myös lähettää sijaintisi ja saada lähistöltäsi seuraavat lähdöt!\n\nJos tarvitset lisää apua tee /help! 😄`, { replyMarkup }); //Vastaa kun käyttäjä käyttää /start komentoa
     return console.log("[info] Start viesti lähetetty!")
 });
 
 // /help
 bot.on('/help', (msg) => {
     //Lähettää viestin
-    bot.sendMessage(msg.from.id, `Hei ${msg.from.first_name}. Täältä löytyy apua!\n\nVoit etsiä pysäkkejä tekemällä ”/hae” ja antamalla pysäkin nimen tai koodin esim.: ”/hae Keilaniemi”. Valitse tämän jälkeen oikea pysäkki näppäimistöstä ja saat pysäkin lähdöt.\n\nVoit myös lähettää sijaintisi painamalla näppäimistöstä ”Sijainnin mukaan 📍” näppäintä. Saat lähistön seuraavat lähdöt.\n\nJos kellonajan perässä on piste, se kertoo että kellonaika on reaaliaikainen ennuste linjan saapumisajasta.\n\nJos löydät bugin tai jotain epätavallista voit reportoida sen kehittäjälle: http://bit.ly/2CBok6s\n\nMukavaa matkaa! 😊`); //Vastaa kun käyttäjä käyttää /start komentoa
+    bot.sendMessage(msg.chat.id, `Hei ${msg.from.first_name}. Täältä löytyy apua!\n\nVoit etsiä pysäkkejä tekemällä ”/hae” ja antamalla pysäkin nimen tai koodin esim.: ”/hae Keilaniemi”. Valitse tämän jälkeen oikea pysäkki näppäimistöstä ja saat pysäkin lähdöt.\n\nVoit myös lähettää sijaintisi painamalla näppäimistöstä ”Sijainnin mukaan 📍” näppäintä. Saat lähistön seuraavat lähdöt.\n\nJos kellonajan perässä on piste, se kertoo että kellonaika on reaaliaikainen ennuste linjan saapumisajasta.\n\nJos löydät bugin tai jotain epätavallista voit reportoida sen kehittäjälle: http://bit.ly/2CBok6s\n\nMukavaa matkaa! 😊`); //Vastaa kun käyttäjä käyttää /start komentoa
     return console.log("[info] Help viesti lähetetty!")
 });
 
 // /menu
 bot.on('/menu', msg => {
     //Lähettää viestin ja näppäimistön
-    bot.sendMessage(msg.from.id, 'Valitse toiminto', { replyMarkup });
+    bot.sendMessage(msg.chat.id, 'Valitse toiminto', { replyMarkup });
     return console.log("[info] Menu avattu!")
 });
-
+/*
 bot.on('/admin', (msg) => {
     return admin.admin(msg.chat.id)
 });
 
 bot.on('/adminhairio', (msg) => {
     return admin.adminhairio(msg.chat.id)
-})
-    
-
+})  
+**/
 bot.on('/hae', msg => {
     return hae(msg.chat.id, msg.text);
 })
-
+/*
 bot.on('/linja', msg => {
     return linja(msg.chat.id, msg.text);
 })
-
+**/
 bot.on(['location'], (msg, self) => {
     return sijainti(msg.chat.id, msg.location);
 });
