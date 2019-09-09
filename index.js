e02d15addeffedf5cd572e11287b0353417f00b9
larmax <leon.miller@hotmail.fi>
2019-09-09 21:19:41 +0300

    korjattu bugi ja siistitty koodia

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // index.js
 
 // Importit
-const bot = require('./bot')
-const replyMarkup = require('./lib/flow/nappaimisto')
-const hae = require('./lib/functions/hae')
-const sijainti = require('./lib/functions/sijainti')
-const linja = require('./lib/functions/linja')
-const poikkeus = require('./lib/functions/poikkeus')
-const pysakkiCheck = require('./lib/functions/pysakkiCheck')
+const bot = require('./bot');
+const replyMarkup = require('./lib/flow/nappaimisto');
+const hae = require('./lib/functions/hae');
+const sijainti = require('./lib/functions/sijainti');
+const linja = require('./lib/functions/linja');
+const poikkeus = require('./lib/functions/poikkeus');
+const pysakkiCheck = require('./lib/functions/pysakkiCheck');
 const pysakki = require('./lib/functions/pysakki');
 const liitynta = require('./lib/functions/liitynta');
 
@@ -23,67 +23,67 @@ bot.on('text', function (msg) {
 //Peruskomennot
 
 bot.on('/start', (msg) => {
-  viimekomento = "/start"
+  viimekomento = "/start";
     //Lähettää viestin ja näppäimistön
     bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Tervetuloa käyttämään pysäkkibottia!\n\nVoit aloittaa käytön kirjoittamalla /hae ja pysäkin nimen tai koodin.\n\nPysäkin sijainnin ja lähdöt saat tekemällä /pysakki ja antamalla pysakin nimen tai koodin.\n\nVoit etsiä aikatauluja tietylle linjalle pysäkiltä tekemällä /linja ja seuraamalla ohjeita\n\nVoit vaihtoehtoisesti myös lähettää sijaintisi ja saada lähistöltäsi seuraavat lähdöt!\n\nJos tarvitset lisää apua tee /help! 😄\n\nTehty käyttäen digitransitin avointa dataa. Digitransit.fi`, { replyMarkup }); //Vastaa kun käyttäjä käyttää /start komentoa
-    return console.log("[info] Start viesti lähetetty!")
+    return console.log("[info] Start viesti lähetetty!");
 });
 
 bot.on('/help', (msg) => {
-  viimekomento = "/help"
+  viimekomento = "/help";
     //Lähettää viestin
     bot.sendMessage(msg.chat.id, `Hei ${msg.from.first_name}. Täältä saa lisätietoa!\n\nKomennot:\n\n/hae - Etsi aikatauluja pysäkin mukaan. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia. Esim: "/hae niittykumpu" tai "/hae E4210"\n\n /pysakki ja /pys - Pysäkin sijainti ja aikataulu. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia: "/pys Kauklahti" tai "/pysakki E4444" \n\n/linja - Etsi aikatauluja linjan perusteella. Anna ensinmäiseksi linjan tunnus, valitse määränpää ja pysäkki. Saat seuraavat lähdöt linjalle pysäkiltä.\n\n/liitynta - Etsi liityntäpysäköintipaikkoja. Esim. "/liitynta Niittykumpu", "/liitynta Hansatie" \n\nVoit lähettää myös sijainnin ja saat lähistöltä seuraavat lähdöt.\n\nSelitteet:\n12:00•‌   = Reaaliaikainen lähtöaika\n12:00!   = Muutoksia reitissä\n12:00×‌  = Vuoro on peruttu\n\nJos löydät bugin tai jotain epätavallista voit reportoida sen kehittäjille: https://bit.ly/2K3GJw1 \n\nMukavaa matkaa! 😊`);
-    return console.log("[info] Help viesti lähetetty!")
+    return console.log("[info] Help viesti lähetetty!");
 });
 
 bot.on('/menu', msg => {
-  viimekomento = '/menu'
+  viimekomento = '/menu';
     //Lähettää viestin ja näppäimistön
     bot.sendMessage(msg.chat.id, 'Valitse toiminto', { replyMarkup });
-    return console.log("[info] Menu avattu!")
+    return console.log("[info] Menu avattu!");
 });
 
 bot.on('/hae', msg => {
-    viimekomento = '/hae'
+    viimekomento = '/hae';
     return hae(msg.chat.id, msg.text);
-})
+});
 
 bot.on('/linja', msg => {
-      viimekomento = '/linja'
+      viimekomento = '/linja';
     return linja(msg.chat.id, msg.text);
-})
+});
 
 bot.on('/poikkeukset', msg => {
-      viimekomento = '/poikkeukset'
+      viimekomento = '/poikkeukset';
     return poikkeus(msg.chat.id);
-})
+});
 
 bot.on(['location'], (msg, self) => {
-      viimekomento = 'location'
+      viimekomento = 'location';
     return sijainti(msg.chat.id, msg.location);
 });
 bot.on('/pys', (msg) => {
-      viimekomento = '/pys'
+      viimekomento = '/pys';
     //Lähettää viestin ja näppäimistön
       return pysakki(msg.chat.id, msg.text);
 });
 bot.on('/pysakki', (msg) => {
-      viimekomento = '/pysakki'
+      viimekomento = '/pysakki';
     //Lähettää viestin ja näppäimistön
       return pysakki(msg.chat.id, msg.text);
 });
 bot.on('/liitynta', (msg) => {
-  viimekomento = "/liitynta"
+  viimekomento = "/liitynta";
     //Lähettää viestin
     return liitynta(msg.chat.id, msg.text);
 });
   bot.on('*', msg => {
-    if (typeof(msg.text) == "string") {
+    if (typeof(msg.text) === "string") {
     setTimeout(function () {
 return pysakkiCheck(msg.chat.id, msg.text, viimekomento);
     }, 10);
 }
-  })
+  });
 
 
 
