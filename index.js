57147d90eae627179c7b568b98c3fafc27aaa555
larmax <leon.miller@hotmail.fi>
2019-09-15 13:00:48 +0300

    paraneltu logauksia

--- index.js
+++ index.js
@@ -26,21 +26,21 @@ bot.on('/start', (msg) => {
   viimekomento = "/start";
     //Lähettää viestin ja näppäimistön
     bot.sendMessage(msg.chat.id, `Hei, ${msg.from.first_name}! Tervetuloa käyttämään pysäkkibottia!\n\nVoit aloittaa käytön kirjoittamalla /hae ja pysäkin nimen tai koodin.\n\nPysäkin sijainnin ja lähdöt saat tekemällä /pysakki ja antamalla pysakin nimen tai koodin.\n\nVoit etsiä aikatauluja tietylle linjalle pysäkiltä tekemällä /linja ja seuraamalla ohjeita\n\nVoit vaihtoehtoisesti myös lähettää sijaintisi ja saada lähistöltäsi seuraavat lähdöt!\n\nJos tarvitset lisää apua tee /help! 😄\n\nTehty käyttäen digitransitin avointa dataa. Digitransit.fi`, { replyMarkup }); //Vastaa kun käyttäjä käyttää /start komentoa
-    return console.log("[info] Start viesti lähetetty!");
+    return console.info("Start viesti lähetetty!");
 });
 
 bot.on('/help', (msg) => {
   viimekomento = "/help";
     //Lähettää viestin
     bot.sendMessage(msg.chat.id, `Hei ${msg.from.first_name}. Täältä saa lisätietoa!\n\nKomennot:\n\n/hae - Etsi aikatauluja pysäkin mukaan. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia. Esim: "/hae niittykumpu" tai "/hae E4210"\n\n /pysakki ja /pys - Pysäkin sijainti ja aikataulu. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia: "/pys Kauklahti" tai "/pysakki E4444" \n\n/linja - Etsi aikatauluja linjan perusteella. Anna ensinmäiseksi linjan tunnus, valitse määränpää ja pysäkki. Saat seuraavat lähdöt linjalle pysäkiltä.\n\n/liitynta - Etsi liityntäpysäköintipaikkoja. Esim. "/liitynta Niittykumpu", "/liitynta Hansatie" \n\nVoit lähettää myös sijainnin ja saat lähistöltä seuraavat lähdöt.\n\nSelitteet:\n12:00•‌   = Reaaliaikainen lähtöaika\n12:00!   = Muutoksia reitissä\n12:00×‌  = Vuoro on peruttu\n\nJos löydät bugin tai jotain epätavallista voit reportoida sen kehittäjille: https://bit.ly/2K3GJw1 \n\nMukavaa matkaa! 😊`);
-    return console.log("[info] Help viesti lähetetty!");
+    return console.info("Help viesti lähetetty!");
 });
 
 bot.on('/menu', msg => {
   viimekomento = '/menu';
     //Lähettää viestin ja näppäimistön
     bot.sendMessage(msg.chat.id, 'Valitse toiminto', { replyMarkup });
-    return console.log("[info] Menu avattu!");
+    return console.info("Menu avattu!");
 });
 
 bot.on('/hae', msg => {
