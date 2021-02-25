3e0ba4c9ac40035a9dce4f54832a5962f820bb33
ainki <alexinkilainen@gmail.com>
2021-02-25 14:53:34 +0200

    Poistettu chatbase testit

--- index.js
+++ index.js
@@ -15,15 +15,7 @@ const reitti = require('./lib/functions/reitti');
 let viimekomennot = [];
 // npm
 require('console-stamp')(console, 'HH:MM:ss'); //Aikaleimat logiin
-// chatbase
-var chatbase = require('@google/chatbase')
 
-const set = chatbase.newMessageSet()
-	// The following are optional setters which will produce new messages with
-	// the corresponding fields already set!
-	.setApiKey(process.env.chatbasetoken)
-  .setPlatform('Telegram')
-  .setVersion('V1.0');
 
 // Logaa jokaisen sisääntulevan viestin consoliin
 bot.on('text', function (msg) {
