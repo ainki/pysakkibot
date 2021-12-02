b5dba20c48ebda86f74725bbacf4faf934650281
Alex Inkiläinen <alexinkilainen@gmail.com>
2021-12-02 13:59:47 +0200

    Lisätty /git komento josta saa linkin tälle github sivulle

--- index.js
+++ index.js
@@ -46,15 +46,22 @@ bot.on('/start', (msg) => {
 viimekomennot = paivitaViimeKomento(viimekomennot,"/start", msg.from.id);
   // viimekomennot.push({komento:"/start", id: msg.from.id});
   //Lähettää viestin ja näppäimistön
-  bot.sendMessage(msg.chat.id, `<b>Hei, ${msg.from.first_name}! Tervetuloa käyttämään Pysäkkibottia!</b>\n\n/hae\nHae aikatauluja pysäkin mukaan. Kirjoita pysäkin nimi tai koodia ja saat pysäkin seuraavat 10 lähtöä.\n\n/reitti  🆕\nHae reittiohjeita paikasta A paikkaan B. Anna vain lähtöpaikka ja määränpää!\n\n/linja\nHae lähöjä tietylle linjalle tietyltä pysäkiltä.\n\n/liitynta\nHae liityntäpysäköinnin tietoja. Realiaikainen data saavilla vasta muutamilla pysäköintialueella.\n\n/pysakki\nHae aikatauluja ja pysäkin sijaintia pysäkin nimen tai koodin perusteella.\n\nVoit myös lähettää sijaintisi ja saat lähistön seuraavat lähdöt.\n\nJos tarvitset lisää apua tee /help! 😄\n\n<b>Kaupunkipyörät @kaupunkipyorabot!</b>`, { replyMarkup, parseMode: 'html' }); //Vastaa kun käyttäjä käyttää /start komentoa
+  bot.sendMessage(msg.chat.id, `<b>Hei, ${msg.from.first_name}! Tervetuloa käyttämään Pysäkkibottia!</b>\n\n/hae\nHae aikatauluja pysäkin mukaan. Kirjoita pysäkin nimi tai koodia ja saat pysäkin seuraavat 10 lähtöä.\n\n/reitti  🆕\nHae reittiohjeita paikasta A paikkaan B. Anna vain lähtöpaikka ja määränpää!\n\n/linja\nHae lähöjä tietylle linjalle tietyltä pysäkiltä.\n\n/liitynta\nHae liityntäpysäköinnin tietoja. Realiaikainen data saavilla vasta muutamilla pysäköintialueella.\n\n/pysakki\nHae aikatauluja ja pysäkin sijaintia pysäkin nimen tai koodin perusteella.\n\nVoit myös lähettää sijaintisi ja saat lähistön seuraavat lähdöt.\n\nJos tarvitset lisää apua tee /help!\nTai löydät ajantasaisimmat ohjeet GitHub sivuilta tekemällä /git 😄\n\n<b>Kaupunkipyörät @kaupunkipyorabot!</b>`, { replyMarkup, parseMode: 'html' }); //Vastaa kun käyttäjä käyttää /start komentoa
   return console.info("Start viesti lähetetty!");
 });
 
 bot.on('/help', (msg) => {
   viimekomennot = paivitaViimeKomento(viimekomennot,"/help", msg.from.id);
   //Lähettää viestin
-  bot.sendMessage(msg.chat.id, `Hei ${msg.from.first_name}. Täältä saa lisätietoa!\n\nKomennot:\n\n/hae - Etsi aikatauluja pysäkin mukaan. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia. Esim: "/hae niittykumpu" tai "/hae E4210"\n\n /pysakki ja /pys - Pysäkin sijainti ja aikataulu. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia: "/pys Kauklahti" tai "/pysakki E4444" \n\n/linja - Etsi aikatauluja linjan perusteella. Anna ensinmäiseksi linjan tunnus, valitse määränpää ja pysäkki. Saat seuraavat lähdöt linjalle pysäkiltä.\n\n/liitynta - Etsi liityntäpysäköintipaikkoja. Esim. "/liitynta Niittykumpu", "/liitynta Hansatie" \n\nVoit lähettää myös sijainnin ja saat lähistöltä seuraavat lähdöt.\n\nSelitteet:\n12:00•‌   = Reaaliaikainen lähtöaika\n12:00!   = Muutoksia reitissä\n12:00×‌  = Vuoro on peruttu\n00:23⁺¹‌‌‎ = Vuoro lähtee seuraavana päivänä \n\nJos löydät bugin tai jotain epätavallista voit reportoida sen kehittäjille: https://bit.ly/2K3GJw1 \n\nMukavaa matkaa! 😊`);
+  bot.sendMessage(msg.chat.id, `Hei ${msg.from.first_name}. Täältä saa lisätietoa!\n\nKomennot:\n\n/hae - Etsi aikatauluja pysäkin mukaan. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia. Esim: "/hae niittykumpu" tai "/hae E4210"\n\n/pysakki ja /pys - Pysäkin sijainti ja aikataulu. Voit joko hakea pysäkkejä nimen mukaan tai käyttää pysäkin koodia: "/pys Kauklahti" tai "/pysakki E4444" \n\n/linja - Etsi aikatauluja linjan perusteella. Anna ensinmäiseksi linjan tunnus, valitse määränpää ja pysäkki. Saat seuraavat lähdöt linjalle pysäkiltä.\n\n/liitynta - Etsi liityntäpysäköintipaikkoja. Esim. "/liitynta Niittykumpu", "/liitynta Hansatie" \n\nVoit lähettää myös sijainnin ja saat lähistöltä seuraavat lähdöt.\n\nSelitteet:\n12:00•‌   = Reaaliaikainen lähtöaika\n12:00!   = Muutoksia reitissä\n12:00×‌  = Vuoro on peruttu\n00:23⁺¹‌‌‎ = Vuoro lähtee seuraavana päivänä \n\n\nTarkemmat ohjeet löydät projektin GitHub sivustoltam tekemällä /git\n\nJos löydät bugin tai jotain epätavallista voit reportoida sen kehittäjille: https://bit.ly/2K3GJw1 \n\nMukavaa matkaa! 😊`);
   return console.info("Help viesti lähetetty!");
+});
+
+bot.on('/git', msg => {
+  viimekomennot = paivitaViimeKomento(viimekomennot,"/git", msg.from.id);
+  //Lähettää viestin ja näppäimistön
+  bot.sendMessage(msg.chat.id, `Hei ${msg.from.first_name}.\n\nOhjeet ja lähdekoodi löytyy osoitteesta https://github.com/ainki/pysakkibot`);
+  return console.info("Git viesti lähetetty avattu!");
 });
 
 bot.on('/menu', msg => {
